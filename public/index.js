47fe4ddadf98335fe6e8877e6b71e915747a4ae1
D4wni <vini.ssantos022010@gmail.com>
2024-12-01 14:17:29 -0300

    Added smthngs

---
+++ public/index.js
@@ -0,0 +1,5 @@
+var openList = document.getElementById('open-list');
+var list = document.getElementById("list-of-false-accounts");
+function handleListClick() {
+    list.style.display = 'block';
+}
---
+++ public/index.ts
@@ -0,0 +1,6 @@
+const openList = document.getElementById('open-list');
+const list = document.getElementById("list-of-false-accounts");
+
+function handleListClick() {
+    list.style.display = 'block';
+}
