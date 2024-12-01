953d1a3596493432888bfc81b2466f184751a967
D4wni <vini.ssantos022010@gmail.com>
2024-12-01 15:53:59 -0300

    Edited index.ts, made responsive style and edited html

--- public/index.js
+++ public/index.js
@@ -1,5 +1,16 @@
+var overlay = document.getElementById("overlay");
 var openList = document.getElementById('open-list');
+var closeList = document.getElementById('close-list');
 var list = document.getElementById("list-of-false-accounts");
-function handleListClick() {
-    list.style.display = 'block';
+function handleOpenListClick() {
+    if (list && overlay) {
+        list.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+}
+function handleCloseListClick() {
+    if (list && overlay) {
+        list.style.display = 'none';
+        overlay.style.display = 'none';
+    }
 }
--- public/index.ts
+++ public/index.ts
@@ -1,6 +1,20 @@
+const overlay = document.getElementById("overlay");
+
 const openList = document.getElementById('open-list');
+const closeList = document.getElementById('close-list');
+
 const list = document.getElementById("list-of-false-accounts");
 
-function handleListClick() {
-    list.style.display = 'block';
+function handleOpenListClick() {
+    if (list && overlay) {        
+        list.style.display = 'block';
+        overlay.style.display = 'block';
+    }
+}
+
+function handleCloseListClick() {
+    if (list && overlay) {        
+        list.style.display = 'none';
+        overlay.style.display = 'none';
+    }
 }
